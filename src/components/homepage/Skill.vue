9fc0f776171eb2df788b64bf81792f68fa7818b4
Fredrik.Aronsson <fredrik.e.aronsson@gmail.com>
2018-09-13 17:34:47 +0200

    fix tooltip

--- src/components/homepage/Skill.vue
+++ src/components/homepage/Skill.vue
@@ -9,11 +9,11 @@
                         <h5>{{type.name}}</h5>
                         <div v-for="(level, idx) in type.level" :key="idx" class="level">
                             <p>{{translation.experience[idx]}}</p>
-                            <div 
-                                v-bind:class="['repeat-' + level, idx]"
-                                v-bind:title="translation.experience[idx] + ': ' + translation.level[level]"
-                            >
-                                <span v-for="a in translation.level"></span>
+                            <div class="experience" v-bind:class="['repeat-' + level, idx]">
+                                <div class="tool-tip">
+                                    {{translation.experience[idx] + ': ' + translation.level[level]}}
+                                </div>
+                                <span v-for="eachLevel in translation.level" :key="eachLevel"></span>
                             </div>
                         </div>
                     </div>
@@ -52,6 +52,7 @@ export default Vue.extend({
 <style lang="sass">
 @import '../../assets/css/variables'
 #skills
+
     h4
         padding: $baseSize
     .level
@@ -61,15 +62,41 @@ export default Vue.extend({
             @for $i from 1 through 5 
                 &.repeat-#{$i} 
                     span:nth-child(-n+#{$i})
-                        border: $blue 1px solid;
-                        background: $blue;
+                        border: $blue 1px solid
+                        background: $blue
             span
-                width: $baseSize;
-                height: $baseSize * .5;
-                margin: ($baseSize / 6) ($baseSize / 4);
-                display: inline-block;
-                border: $blue 1px solid;
+                width: $baseSize
+                height: $baseSize * .5
+                margin: ($baseSize / 6) ($baseSize / 4)
+                display: inline-block
+                border: $blue 1px solid
                 border-radius: 2px
+        .experience
+            position: relative
+            .tool-tip
+                display: none
+                font-size: ($baseSize * .75)
+                background: $blue
+                position: absolute
+                top: -($baseSize * 3)
+                left: 0
+                padding: ($baseSize / 4) ($baseSize / 2)
+                border-radius: $baseSize
+                color: $lightBlue
+                &:after
+                    content: ""
+                    position: absolute
+                    top: 100%
+                    left: 50%
+                    margin-left: -5px
+                    border-width: 5px
+                    border-style: solid
+                    border-color: $blue transparent transparent transparent
+        &:hover
+            .experience
+                .tool-tip 
+                    display: inline-block
+
 
 
 </style>
--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,20 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
+Vue.prototype.$label = {
+  readMore: 'Read more →',
+  preview: 'Preview now ↝',
+  translation: {
+    "experience": {
+        "personalExp": "Personal Experience",
+        "interest": "Personal Interest",
+        "workExp": "Work-related Experience"
+    },
+    "level": {
+        1: 'Beginner',
+        2: 'Intermediate',
+        3: 'Advanced',
+        4: 'Expert'
+    }
+  }
+}
